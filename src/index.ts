e763502e90c270d323bb7cc0e9f0a4e165391774
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-06-25 16:26:46 -0300

    feat: adding docker files, package.json and initial index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+});
