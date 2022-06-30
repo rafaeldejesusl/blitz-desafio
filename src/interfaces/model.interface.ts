850b709a9ebf54946fb8fbb0696e148473384952
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-06-30 11:59:08 -0300

    feat: add erase in interfaces

--- src/interfaces/model.interface.ts
+++ src/interfaces/model.interface.ts
@@ -2,7 +2,8 @@ import ITask from './task.interface';
 
 interface ITaskModel {
   create(task: ITask): Promise<ITask>;
-  getAll(): Promise<ITask[]>
+  getAll(): Promise<ITask[]>;
+  erase(id: number): Promise<{ id: number }>
 }
 
 export default ITaskModel;
--- src/interfaces/service.interface.ts
+++ src/interfaces/service.interface.ts
@@ -2,7 +2,8 @@ import ITask from './task.interface';
 
 interface ITaskService {
   create(task: ITask): Promise<ITask>;
-  getAll(): Promise<ITask[]>
+  getAll(): Promise<ITask[]>;
+  erase(id: number): Promise<{ id: number }>
 }
 
 export default ITaskService;
