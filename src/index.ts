5e632b33853ad3c8e56348d00fd240fd68deed91
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-07-01 00:47:12 -0300

    feat: add error treatment

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import TaskRoutes from './routes/task.routes';
+import ErrorHandler from './interfaces/error.interface';
+import 'express-async-errors';
 
 const app = express();
 
@@ -8,6 +10,14 @@ app.use(express.json());
 app.use(TaskRoutes);
 
 const PORT = 3000;
+
+app.use((err: ErrorHandler, req: Request, res: Response, _next: NextFunction) => {
+  const { status, message } = err;
+  if (status) {
+    return res.status(status).json({ message });
+  }
+  return res.status(500).json({ message });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
