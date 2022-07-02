80437cb89684cbe7eecdf11147d5e28f33f5059c
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-07-02 15:41:23 -0300

    feat: installing cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,14 @@ import express, { NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import TaskRoutes from './routes/task.routes';
 import ErrorHandler from './interfaces/error.interface';
+import cors from 'cors';
 import 'express-async-errors';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
