fd7b1530bff98f20e6f79e3cd072d36bc6bd87cd
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-07-02 23:04:06 -0300

    feat: add home component

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import TaskList from './TaskList';
+import Form from './Form';
+
+function Home() {
+  const [tasks, setTasks] = useState([]);
+  const api = axios.create({
+    baseURL: 'http://localhost:3001',
+  });
+
+  const fetchTasks = async () => {
+    try {
+      const results = await api.get('/tasks');
+      const data = await results.data;
+      setTasks(data);
+    } catch (error) {
+      window.alert(error.response.data.message);
+    }
+  };
+
+  const saveTask = async (name, status) => {
+    try {
+      await api.post('/tasks', { name, status });
+      await fetchTasks();
+    } catch (error) {
+      window.alert(error.response.data.message);
+    }
+  };
+
+  const updateTask = async (status, id) => {
+    try {
+      await api.put(`/tasks/${id}`, { status });
+      await fetchTasks();
+    } catch (error) {
+      window.alert(error.response.data.message);
+    }
+  };
+
+  const removeTask = async (id) => {
+    try {
+      await api.delete(`/tasks/${id}`);
+      await fetchTasks();
+    } catch (error) {
+      window.alert(error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista de Tarefas</h1>
+      <Form saveTask={saveTask} updateTask={updateTask} />
+      <TaskList tasks={tasks} removeTask={removeTask} />
+    </div>
+  );
+}
+
+export default Home;
