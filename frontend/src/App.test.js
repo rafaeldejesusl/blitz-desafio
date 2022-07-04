062c81146aac81a90bc629fd2c2b0c6ae3f4d953
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-07-04 18:18:50 -0300

    feat: add order feature

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -16,9 +16,9 @@ describe('Ao renderizar a página', () => {
 
   test('contém a tabela', () => {
     render(<App />);
-    const taskColumn = screen.getByRole('cell', { name: 'Tarefa' });
-    const createdAtColumn = screen.getByRole('cell', { name: 'Criado em' });
-    const statusColumn = screen.getByRole('cell', { name: 'Status' });
+    const taskColumn = screen.getByRole('button', { name: 'Tarefa' });
+    const createdAtColumn = screen.getByRole('button', { name: '⇓Criado em' });
+    const statusColumn = screen.getByRole('button', { name: 'Status' });
     const editColumn = screen.getByRole('cell', { name: 'Editar' });
     const removeColumn = screen.getByRole('cell', { name: 'Remover' });
 
@@ -85,5 +85,87 @@ describe('A página permite', () => {
     });
 
   });
-})
+});
+
+describe('A página permite organização', () => {
+  test('inicial por ordem de criação', async () => {
+    render(<App />);
+    const nameInput = screen.getByLabelText('Tarefa:');
+    const statusInput = screen.getByLabelText('Status:');
+    const submitButton = screen.getByRole('button', { name: 'Criar Tarefa' });
+
+    userEvent.type(nameInput, 'Programar');
+    userEvent.selectOptions(statusInput, 'Pendente');
+    userEvent.click(submitButton);
+    userEvent.clear(nameInput);
+    userEvent.type(nameInput, 'Estudar');
+    userEvent.selectOptions(statusInput, 'Em Andamento');
+    userEvent.click(submitButton);
+    userEvent.clear(nameInput);
+    userEvent.type(nameInput, 'Dormir');
+    userEvent.selectOptions(statusInput, 'Pronto');
+    userEvent.click(submitButton);
+
+    const rowHeaders = await screen.findAllByTestId('row-header');
+
+    expect(rowHeaders[0]).toHaveTextContent('Programar');
+    expect(rowHeaders[1]).toHaveTextContent('Estudar');
+    expect(rowHeaders[2]).toHaveTextContent('Dormir');
+  });
+
+  test('por ordem do nome da tarefa', async () => {
+    render(<App />);
+    const taskColumn = screen.getByRole('button', { name: 'Tarefa' });
+
+    userEvent.click(taskColumn);
+
+    const rowHeaders = await screen.findAllByTestId('row-header');
+
+    expect(rowHeaders[0]).toHaveTextContent('Dormir');
+    expect(rowHeaders[1]).toHaveTextContent('Estudar');
+    expect(rowHeaders[2]).toHaveTextContent('Programar');
+  });
+
+  test('por ordem do status da tarefa', async () => {
+    render(<App />);
+    const statusColumn = screen.getByRole('button', { name: 'Status' });
+
+    userEvent.click(statusColumn);
+
+    const rowHeaders = await screen.findAllByTestId('row-header');
+
+    expect(rowHeaders[0]).toHaveTextContent('Estudar');
+    expect(rowHeaders[1]).toHaveTextContent('Programar');
+    expect(rowHeaders[2]).toHaveTextContent('Dormir');
+  });
+
+  test('por ordem de criação da tarefa', async () => {
+    render(<App />);
+    const createdAtColumn = screen.getByRole('button', { name: '⇓Criado em' });
+
+    userEvent.click(createdAtColumn);
+
+    const rowHeaders = await screen.findAllByTestId('row-header');
+
+    expect(rowHeaders[0]).toHaveTextContent('Programar');
+    expect(rowHeaders[1]).toHaveTextContent('Estudar');
+    expect(rowHeaders[2]).toHaveTextContent('Dormir');
+
+    const removeButton = await screen.findAllByRole('button', { name: 'X' });
+
+    userEvent.click(removeButton[0]);
+
+    userEvent.click(removeButton[1]);
+
+    userEvent.click(removeButton[2]);
+
+    await waitFor(() => {
+
+      expect(rowHeaders[2]).not.toBeInTheDocument();
+    });
+
+    render(<App />);
+  });
+});
+
 
